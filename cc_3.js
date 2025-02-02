2be927047bb27b8f44723d289076065af6ddf196
Casey Tomchuk <Caseytomchuk@gmail.com>
2025-02-02 14:27:51 -0500

    Task 1 - Product Price Management

---
+++ cc_3.js
@@ -0,0 +1,5 @@
+// Task 1: Product Price Management Scenario: Online Store You are managing a product pricing system.
+let prices = [20,17,25,15,12];
+prices.push(21);
+prices.shift();
+console.log("Prices: " + prices);
