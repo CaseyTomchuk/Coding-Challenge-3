584f9fbbe3e490549e1e96876e68736ed598318a
Casey Tomchuk <Caseytomchuk@gmail.com>
2025-02-02 14:44:52 -0500

    Task 2 - Modifying Customer Orders

--- cc_3.js
+++ cc_3.js
@@ -3,3 +3,17 @@ let prices = [20,17,25,15,12];
 prices.push(21);
 prices.shift();
 console.log("Prices: " + prices);
+
+// Task 2 Modifying Customer Orders Scenario: Order List You are processing customer orders and need to update order quantities.
+let orders = [43,27,40,34,38];
+orders[2] += 5; //adding 5 orders to the third item in the array
+
+let sum = 0;
+for (let i = 0; i < orders.length; i++) {     
+    sum += orders[i];
+}
+//this for loop works by calculating the sum of all items in the array. after running, i++ moves the loop on to the next item.
+// i < orders.length repeats the process if there are more items left in the array. sum += orders[i] is how the summing is done
+
+console.log("List of orders: " + orders);
+console.log("Total number of orders: " + sum);
